122844c83867d1b1a9583970b95356ae58c95dff
rsanting <rene@santing.net>
2025-01-15 21:12:12 +0100

    Update admin and add user admin

--- schedulr/account/admin.py
+++ schedulr/account/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from unfold.forms import (AdminPasswordChangeForm, UserChangeForm,
+                          UserCreationForm)
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+    list_display = ('email', 'is_staff', 'is_active')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),)
