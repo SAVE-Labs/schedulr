35bb446f99da7330f8e0b3c927fd95d0708e7b46
Marco Vellinga <marco@vellinga.dev>
2025-01-29 22:24:14 +0100

    Make admin add work

--- schedulr/account/admin.py
+++ schedulr/account/admin.py
@@ -1,23 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 from unfold.admin import ModelAdmin
-from unfold.forms import (AdminPasswordChangeForm, UserChangeForm,
-                          UserCreationForm)
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(ModelAdmin):
+class UserAdmin(BaseUserAdmin, ModelAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
     change_password_form = AdminPasswordChangeForm
 
-    list_display = ('email', 'is_staff', 'is_active')
-    ordering = ('email',)
+    list_display = ("email", "is_staff", "is_active")
+    ordering = ("email",)
 
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),)
+    fieldsets = ((None, {"fields": ("email", "password")}),)
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "usable_password",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("email",)
+    ordering = ("id",)
+
 
 # Unregister the Group model from admin as it's not used
 admin.site.unregister(Group)
